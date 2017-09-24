a0352c493f6d41abb2326927b7df0bb6f6bfd421
J Da Camara <jehandacamara@hotmail.com>
2017-09-24 21:29:17 +0200

    Virus total report 4 and 5 and error handeling for the last api call.

--- data/virustotal_check.py
+++ data/virustotal_check.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     parts_size = size / 6
     domains = domains[part * parts_size: None if part == 5 else (part + 1) * parts_size]
 
-    SHARIF_API_KEY1 = 'd129d0c6cdd3065c02247967ea5111c8e0b37f43db4491be4856eb00fad1717b'
+    SHARIF_API_KEY1 = '9e390226d782b837cfdfbc77b8c77bc60185f966d08993fb365e569b1f893bdb'
     url = 'https://www.virustotal.com/vtapi/v2/url/report'
 
     limit = None
@@ -31,20 +31,23 @@ if __name__ == '__main__':
     results = []
 
     for i, domain in enumerate(domains):
-        if limit and succes >= limit:
+        try :
+            if limit and succes >= limit:
+                break
+            if domain in ('-', ''):
+                continue
+            if succes > 0 and succes % 4 == 0:
+                print '(%d/%d)...' % (i, parts_size)
+                time.sleep(61)
+            params = {'apikey': SHARIF_API_KEY1, 'resource': domain}
+            response = requests.get(url, params=params)
+            json_response = response.json()
+            result = '%s: %d' % (domain, json_response['positives'])
+            results.append(result)
+            print result
+            succes += 1
+        except:
             break
-        if domain in ('-', ''):
-            continue
-        if succes > 0 and succes % 4 == 0:
-            print '(%d/%d)...' % (i, parts_size)
-            time.sleep(61)
-        params = {'apikey': SHARIF_API_KEY1, 'resource': domain}
-        response = requests.get(url, params=params)
-        json_response = response.json()
-        result = '%s: %d' % (domain, json_response['positives'])
-        results.append(result)
-        print result
-        succes += 1
 
     with open('virustotal_report_%d.txt' % part, 'w') as f:
         for result in results:
